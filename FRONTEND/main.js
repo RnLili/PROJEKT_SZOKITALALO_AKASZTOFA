be0f45211c5b8fde124d2bb37cd920e6de19cc52
ruth.nora.lili@stud.uni-obuda.hu <ruth.nora.lili@stud.uni-obuda.hu>
2025-05-09 23:36:23 +0200

    Stylesheet módosítás

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,7 +1,7 @@
 async function download()
 {
     const response = await fetch('http://localhost:5278/word/GetRandomWord');
-    let data = await response.json(); //eddig const volt
+    let data = await response.json(); 
     let word = data.word;
     let inputNum = word.length;
     let container = document.querySelector("#inputs")
@@ -11,6 +11,7 @@ async function download()
     console.log(inputNum)
     for (let index = 0; index < inputNum; index++) {
         let input = document.createElement('input')
+        input.setAttribute('class', 'letter-input');
         input.setAttribute('type', 'text');
         input.setAttribute('maxlength', '1');
         container.appendChild(input);
